696f0563ef1c79aae861f574199293697effacaf
Eduardo Fernández (Mac) <feguedi@yahoo.com.mx>
2021-08-05 18:31:55 -0500

    Terminada pantalla de scroll

--- lib/src/views/scroll_design.dart
+++ lib/src/views/scroll_design.dart
@@ -1,44 +1,82 @@
 import 'package:flutter/material.dart';
 
 class ScrollDesign extends StatelessWidget {
-  const ScrollDesign({Key? key}) : super(key: key);
+  final textStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 60,
+    color: Colors.white70,
+  );
 
   @override
   Widget build(BuildContext context) {
-    final textStyle = TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 60,
-      color: Colors.white70,
-    );
     return Scaffold(
-      body: Stack(
+      body: PageView(
+        // physics: BouncingScrollPhysics(), // iOS scroll
+        physics: ClampingScrollPhysics(), // Android scroll
+        scrollDirection: Axis.vertical,
         children: [
-          _background(),
-          SafeArea(
-            bottom: false,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: 40),
-                Text('11º', style: textStyle),
-                Text('Miércoles', style: textStyle),
-                Expanded(child: Container()),
-                Icon(Icons.keyboard_arrow_down,
-                    size: 100, color: Colors.white70),
-              ],
-            ),
-          ),
+          _scrollPage1(),
+          _scrollPage2(),
         ],
       ),
     );
   }
 
-  _background() => Container(
-        color: Color(0xff30bad6),
-        child: Image(
-          image: AssetImage('assets/scroll-1.png'),
-          fit: BoxFit.cover,
+  Stack _scrollPage1() {
+    return Stack(
+      children: [
+        _background(),
+        _mainContent(),
+      ],
+    );
+  }
+
+  Container _background() {
+    return Container(
+      color: Color(0xff30bad6),
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  SafeArea _mainContent() {
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 40),
+          Text('11º', style: textStyle),
+          Text('Miércoles', style: textStyle),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 100, color: Colors.white70),
+        ],
+      ),
+    );
+  }
+
+  Container _scrollPage2() {
+    return Container(
+      color: Color(0xff30bad6),
+      child: Center(
+        child: TextButton(
+          onPressed: () {},
+          child: Text(
+            'Bienvenido',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            ),
+          ),
+          style: TextButton.styleFrom(
+              backgroundColor: Color(0xff0098FA),
+              shape: StadiumBorder(),
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10)),
         ),
-      );
+      ),
+    );
+  }
 }
