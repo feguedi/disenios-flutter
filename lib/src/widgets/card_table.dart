62776e5810e4d5ee5b8e7f5f6b763c32278be6e5
Eduardo Fernández (Mac) <feguedi@yahoo.com.mx>
2021-08-05 22:35:11 -0500

    Pequeña optimización del código para que sea más fácil de leer

--- lib/src/widgets/card_table.dart
+++ lib/src/widgets/card_table.dart
@@ -1,6 +1,4 @@
 import 'dart:ui';
-
-import 'package:disenios/src/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class CardTable extends StatelessWidget {
@@ -87,38 +85,78 @@ class _SingleCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         child: BackdropFilter(
           filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(62, 66, 107, 0.7),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            padding: EdgeInsets.all(20),
-            height: 180,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  decoration: boxDecoration,
-                  child: CircleAvatar(
-                    backgroundColor: Colors.transparent,
-                    radius: 30,
-                    child: Icon(
-                      icon,
-                      size: 35,
-                      color: Colors.white70,
-                    ),
-                  ),
-                ),
-                Text(
-                  text,
-                  style: TextStyle(
-                    color: color,
-                    fontSize: 16,
-                  ),
-                ),
-              ],
+          child: _CardContent(
+              boxDecoration: boxDecoration,
+              icon: icon,
+              text: text,
+              color: color),
+        ),
+      ),
+    );
+  }
+}
+
+class _CardContent extends StatelessWidget {
+  const _CardContent({
+    Key? key,
+    required this.boxDecoration,
+    required this.icon,
+    required this.text,
+    required this.color,
+  }) : super(key: key);
+
+  final BoxDecoration boxDecoration;
+  final IconData icon;
+  final String text;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(62, 66, 107, 0.7),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      padding: EdgeInsets.all(20),
+      height: 180,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _CardBackground(boxDecoration: boxDecoration, icon: icon),
+          Text(
+            text,
+            style: TextStyle(
+              color: color,
+              fontSize: 16,
             ),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CardBackground extends StatelessWidget {
+  const _CardBackground({
+    Key? key,
+    required this.boxDecoration,
+    required this.icon,
+  }) : super(key: key);
+
+  final BoxDecoration boxDecoration;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: boxDecoration,
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 30,
+        child: Icon(
+          icon,
+          size: 35,
+          color: Colors.white70,
         ),
       ),
     );
